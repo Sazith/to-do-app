e07168600e24e20713f7eec62680dddbc9229c48
Sazith <sazith.simecsoftdev@gmail.com>
2023-12-04 16:50:25 +0600

    page design done

---
+++ script.js
@@ -0,0 +1,11 @@
+/*
+ * Title: To Do Application using vanilla JS DOM
+ * Description: This JS file has all the JS functions necessary to control the to do application
+ * Author: Md. Sazith Shyonton
+ * Date: 04/12/2023
+ *
+ */
+
+
+
+ 
